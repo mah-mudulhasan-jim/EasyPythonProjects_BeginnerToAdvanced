b28aad32852b448e688766fe120f2b24254ec5fe
mhasanjim <mhd.mahmudulhasanjim@gmail.com>
2025-06-04 06:54:08 +0600

    Quiz 4th June '25

--- p14_Quiz/main.py
+++ p14_Quiz/main.py
@@ -3,12 +3,9 @@ from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
 
-
-
 question_bank = []
 for question in question_data:
     question_bank.append(Question(question["text"], question["answer"]))
-
 
 quiz = QuizBrain(question_bank)
 
